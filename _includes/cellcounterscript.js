1d1878ae371b6a67a3fd3a113b8b0a22c6bb4988
Jennifer <52807299+hiJen14@users.noreply.github.com>
2019-07-16 18:40:31 +0200

    Update and rename cellcounterscript.js to scaleCells.js

--- _includes/cellcounterscript.js
+++
@@ -1,9 +0,0 @@
-var cellCounter = 0;
-
-window.onload = function myFunction()
-{
-    var name = prompt("What's your name?");
-    var lengthOfName = name.length
-
-    document.getElementById('output').innerHTML = lengthOfName;
-};
---
+++ _includes/scaleCells.js
@@ -0,0 +1,12 @@
+//Function to rock the cells. Or scale them!
+window.onload = function myFunction()
+{
+    var cellID = document.getElementById("cell1");
+    console.log(cellID.outerHTML);
+    
+    
+    //var name = prompt("What's your name?");
+    //var lengthOfName = name.length
+
+   // document.getElementById('output').innerHTML = lengthOfName;
+};
