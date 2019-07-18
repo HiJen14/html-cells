848c9061e3d88346adc7fc1cd37acad7bdf3afec
Jennifer <52807299+hiJen14@users.noreply.github.com>
2019-07-18 14:46:27 +0200

    cleaning of code/ temp dir

--- scripts/scaleCells.js
+++ scripts/scaleCells.js
@@ -1,53 +1,8 @@
 /*Script to rock the cells. Or scale them!*/
 /******************************************/
 
-    /*window.onload = function myFunction()
-    {
-        var cellID = document.getElementById("cell1");
-        console.log(cellID.outerHTML);
-    };*/
-
 function InitializeCells(totalMainCells, cellsOnRow, xmlUrl) 
 {
-   //Read XML file
-    /*var xmlCellConfig = "";
-    var objXMLHttpRequest = new XMLHttpRequest();
-    objXMLHttpRequest.onreadystatechange = function () 
-	{
-		if (objXMLHttpRequest.readyState == 4 && objXMLHttpRequest.status == 200) 
-		{
-			xmlCellConfig = objXMLHttpRequest.responseXML;
-			console.log(xmlCellConfig);
-        }
-    };
-    objXMLHttpRequest.open("GET", xmlUrl, true);
-    objXMLHttpRequest.send();
-	
-	var parser, xml;
-	//All browsers 
-	if (window.DOMParser) 
-	{
-		parser = new DOMParser();
-		xml = parser.parseFromString(xmlCellConfig, 'text/xml');
-	}
-	else 
-	{ // For internet explorer
-		xml = new ActiveXObject('Microsoft.XMLDOM');
-		xml.async = false;
-		xml.loadXML(xmlCellConfig);
-	}
-
-	var nodes = xml.getElementsByTagName('cell');
-	
-		console.log(nodes);
-
-	var i, l = nodes.length, answers = [];
-	for (i = 0; i < l; i++) {
-		answers.push(nodes[i].childNodes[0].nodeValue);
-	}
-
-	console.log(answers);*/
-	
     console.log(widthCells);
     
     var percentageOfElement = 100 / cellsOnRow;
