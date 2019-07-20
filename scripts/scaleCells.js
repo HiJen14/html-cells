a46394654faf63c9c28fd8b23575fe8db80410a2
Jennifer <52807299+hiJen14@users.noreply.github.com>
2019-07-20 14:54:18 +0200

    Update scaleCells.js

--- scripts/scaleCells.js
+++ scripts/scaleCells.js
@@ -12,19 +12,11 @@ function InitializeCells(totalMainCells, cellsOnRow, xmlUrl)
     ********************************************
     */
     
-    var biggestHeight = 0; 
 	var totalWidthCount = 0;
     //look at all cells in a "row". 
     //A row isn't defined by <div>, but by cellsOnRow
     for (var i = 1; i <= cellsOnRow; i++) 
-    {
-        var elementHeight = document.getElementById("cell" + i.toString()).clientHeight;
-         console.log(elementHeight);
-        if(elementHeight > biggestHeight)
-        {
-            biggestHeight = elementHeight;
-        }
-		
+    {	
 		/*Count up the widthCells percentage count*/
 		totalWidthCount += parseInt(widthCells[i-1]);
 		console.log(totalWidthCount);
@@ -33,8 +25,8 @@ function InitializeCells(totalMainCells, cellsOnRow, xmlUrl)
 	
 	//  100% / totalWidthCount 
 	var percentage = 100 / totalWidthCount;
-	console.log(percentage);
     
+	/*Put width on <div id="cell">*/
     for (i = 1; i <= cellsOnRow; i++) 
     {	
         var element = document.getElementById("cell" + i.toString());
@@ -42,7 +34,28 @@ function InitializeCells(totalMainCells, cellsOnRow, xmlUrl)
 		var percentageOfElement = parseInt(widthCells[i-1]) * percentage;
 		console.log(percentageOfElement);
         element.style.width =  percentageOfElement.toString() + "%";
+    }	
+	
+	var biggestHeight = 0; 
+    //look at all cells in a "row". 
+    //A row isn't defined by <div>, but by cellsOnRow
+    for (var i = 1; i <= cellsOnRow; i++) 
+    {
+		/*Find biggest element height*/
+        var elementHeight = document.getElementById("cell" + i.toString()).clientHeight;
+         console.log(elementHeight);
+        if(elementHeight > biggestHeight)
+        {
+            biggestHeight = elementHeight;
+        }
+    }
+	
+	/*Put height on <div id="cell">*/
+	for (i = 1; i <= cellsOnRow; i++) 
+    {	
+        var element = document.getElementById("cell" + i.toString());
 		
         element.style.height = biggestHeight.toString() + "px";
     }	
+	
 }
